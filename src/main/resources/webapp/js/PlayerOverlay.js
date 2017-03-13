50ed1329cb06892072f1dcb0642de9e964c0d907
Jørgen Braseth <jorgen.braseth@gmail.com>
2017-03-13 09:43:46 +0100

    Removed some debug logging.

--- src/main/resources/webapp/js/PlayerOverlay.js
+++ src/main/resources/webapp/js/PlayerOverlay.js
@@ -8,9 +8,6 @@ class PlayerOverlay {
         ctx.strokeStyle = "white";
         var width = 50 + Math.max(... players.map(p => ctx.measureText(p.name).width + ctx.measureText(p.resources.resource).width));
         var height = players.length * 20 + 15;
-        console.log(height);
-        console.log(width);
-        console.log(... players.map(p => ctx.measureText(p.name).width + ctx.measureText(p.resources.resource).width));
         ctx.fillRect(-1, -1, width + 1, height + 1);
         ctx.strokeRect(-1, -1, width + 1, height + 1);
 
